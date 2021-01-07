37728ff370ead549e359e51867b01eab06f2eb60
CAHevs <48435770+CAHevs@users.noreply.github.com>
2021-01-07 11:16:21 +0100

    add/change image done and working

--- lib/person_details.dart
+++ lib/person_details.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gmoria_grp4/Objects/Users.dart';
+import 'package:image_picker/image_picker.dart';
 
 //Class for the page with all the information regarding a person
 class PersonDetails extends StatelessWidget{
@@ -7,14 +13,27 @@ class PersonDetails extends StatelessWidget{
   final Users selectedUser; 
   PersonDetails(this.selectedUser); 
 
+  var notes; 
+  String _image;
+  File _storedImage;
+  final picker = ImagePicker();
+
   @override
   Widget build(BuildContext context) {
    var title = selectedUser.firstname + " " + selectedUser.lastname; 
+   _image = selectedUser.image;
    return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(child:  buildAllInformation(),),
+      body: Center(child:  buildAllInformation(),
+      ),
+      floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              savePerson();
+            }, 
+            child: Icon(Icons.save),
+      ),
     );
   }
 
@@ -24,22 +43,64 @@ class PersonDetails extends StatelessWidget{
     children: <Widget>[
       ClipRRect(
           borderRadius: BorderRadius.circular(20),
-          child: Image.network(selectedUser.image, height: 300, width: 300,)
+          child: Image.file(File(_image), height: 300, width: 300,)
       ),
+      IconButton(icon: Icon(Icons.add_a_photo), 
+      onPressed: () {
+        getImage();
+        print(_image);
+      }),
       Container(width: 300, 
       padding: EdgeInsets.all(30),
       child: 
-        TextField(
+        TextFormField(
           keyboardType: TextInputType.multiline, 
           maxLines: null, 
+          initialValue: selectedUser.getNote(),
+          onChanged: (value){
+            notes = value; 
+          },
           decoration: new InputDecoration(
             border: new OutlineInputBorder(
               borderSide: new BorderSide(color: Colors.grey)
             ),
-            hintText: 'Add notes regarding the person',
-            labelText: 'Notes'
           ),)
-      )
+      ),
     ],
   );
+
+  //Method that will add/edit notes for a person
+  Future<void> savePerson(){
+    return FirebaseFirestore.instance
+      .collection(FirebaseAuth.instance.currentUser.email)
+      .doc('users')
+      .collection('users')
+      .doc(selectedUser.getId())
+      .update({'note': notes, 'image':_image});
+  }
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    print(pickedFile.path);
+    if(pickedFile != null){
+      final String fileName = basename(pickedFile.path);
+      print(fileName);
+      var appDir;
+      try{
+         appDir = await getApplicationDocumentsDirectory();
+      }on Exception catch(e){
+        print(e);
+      }
+      
+      final appDocPath = appDir.path;
+      print(appDocPath);
+      final filePath = '$appDocPath/$fileName';
+      print(filePath);
+      final File selectedImage = File(pickedFile.path);
+      final File localImage = await selectedImage.copy('$filePath');
+      _image = localImage.path;
+    }else{
+      _image = selectedUser.image;
+    }
+  }
 }
